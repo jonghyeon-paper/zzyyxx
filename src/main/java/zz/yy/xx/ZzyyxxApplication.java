14ac6a33b8e281c20c1e2a9eb0037197f3f1a4cf
jonghyeon-paper <lieniece@gmail.com>
2025-02-27 15:45:30 +0900

    Create ZzyyxxApplication.java
    add class

---
+++ src/main/java/zz/yy/xx/ZzyyxxApplication.java
@@ -0,0 +1,18 @@
+package zz.yy.xx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ZzyyxxApplication:
+ * 
+ * <br>
+ * created by good 2025.02.
+ */
+@SpringBootApplication
+public class ZzyyxxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzyyxxApplication.class, args);
+    }
+}
