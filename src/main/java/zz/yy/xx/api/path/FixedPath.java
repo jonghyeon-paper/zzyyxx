95fe7287f47a46d7e92d587c578a09d96928027a
jonghyeon-paper <lieniece@gmail.com>
2025-03-04 17:48:31 +0900

    Create FixedPath.java

---
+++ src/main/java/zz/yy/xx/api/path/FixedPath.java
@@ -0,0 +1,12 @@
+package zz.yy.xx.api.path;
+
+/**
+ * FixedPath:
+ * 
+ * <br>
+ * created by good on 2025.03.
+ */
+public class FixedPath {
+
+    public static final String VERSION_PATH = "/version01";
+}
