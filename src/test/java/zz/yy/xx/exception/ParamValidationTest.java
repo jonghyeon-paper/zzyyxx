f9558e13427f53a4bf678cd784fb62c59dbd7ea8
jonghyeon-paper <lieniece@gmail.com>
2025-03-04 17:34:09 +0900

    Create ParamValidationTest.java

---
+++ src/test/java/zz/yy/xx/exception/ParamValidationTest.java
@@ -0,0 +1,56 @@
+package zz.yy.xx.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ParamValidationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void usingAnnotation() throws Exception {
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        // parameters.add("key", "123");
+        parameters.add("value", "qwe");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/sample/validation-using-annotation")
+                .params(parameters)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest())
+        .andDo((result) -> {
+            System.out.println("-------finish--------");
+            System.out.println(result.getResolvedException()); 
+            System.out.println(result.getResponse().getContentAsString()); 
+        });
+    }
+
+    @Test
+    public void usingCustom() throws Exception {
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        // parameters.add("key", "123");
+        parameters.add("value", "qwe");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/sample/validation-using-custom")
+                .params(parameters)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andDo((result) -> {
+                    System.out.println("-------finish--------");
+                    System.out.println(result.getResolvedException()); 
+                    System.out.println(result.getResponse().getContentAsString()); 
+                });
+    }
+}
