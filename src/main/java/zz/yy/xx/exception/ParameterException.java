09b80e81b43f311b4278a875192422498e23d5c0
jonghyeon-paper <lieniece@gmail.com>
2025-03-04 17:38:13 +0900

    Create ParameterException.java

---
+++ src/main/java/zz/yy/xx/exception/ParameterException.java
@@ -0,0 +1,16 @@
+package zz.yy.xx.exception;
+
+/**
+ * ParameterException:
+ * 
+ * <br>
+ * created by good on 2025.03.
+ */
+public class ParameterException extends RuntimeException {
+
+    private static final long serialVersionUID = 3846154483793443964L;
+
+    public ParameterException(String message) {
+        super(message);
+    }
+}
