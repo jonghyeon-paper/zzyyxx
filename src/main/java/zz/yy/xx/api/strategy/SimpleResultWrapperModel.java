c2ba666854324cce3920b5f6ed050017de122a39
jonghyeon-paper <lieniece@gmail.com>
2025-03-05 16:02:04 +0900

    Create SimpleResultWrapperModel.java

---
+++ src/main/java/zz/yy/xx/api/strategy/SimpleResultWrapperModel.java
@@ -0,0 +1,14 @@
+package zz.yy.xx.api.strategy;
+
+/**
+ * SimpleResultWrapperModel:
+ * 
+ * <br>
+ * created by good on 2025.03.
+ * 
+ * @deprecated unused
+ */
+@Deprecated
+public class SimpleResultWrapperModel {
+
+}
