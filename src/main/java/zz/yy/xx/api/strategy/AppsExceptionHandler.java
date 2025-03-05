90bab9c3e5db31de1dfbc36e795ddd7572b187d8
jonghyeon-paper <lieniece@gmail.com>
2025-03-05 16:03:32 +0900

    Create AppsExceptionHandler.java

---
+++ src/main/java/zz/yy/xx/api/strategy/AppsExceptionHandler.java
@@ -0,0 +1,32 @@
+package zz.yy.xx.api.strategy;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * AppsExceptionHandler:
+ * 
+ * <br>
+ * created by good on 2025.03.
+ */
+@Controller
+@ControllerAdvice
+public class AppsExceptionHandler {
+
+    /**
+     * handleBusinessException:
+     * 
+     * @param exception
+     * @return
+     */
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseBody
+    public ResponseEntity<SimpleFailureResponseModel> handleBusinessException(RuntimeException exception) {
+        AppsResponse respnose = AppsResponse.FAILURE_UNKNOWN;
+        return ResponseEntity.status(respnose.getHttpStatus()).body(SimpleFailureResponseModel
+                .of(respnose.getExtraCode(), respnose.getExtraText() + ":\n" + exception.getMessage()));
+    }
+}
