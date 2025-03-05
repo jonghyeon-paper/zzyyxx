a58aa3a9498e1e4e4b7f4d41d60f6bd9b8033603
jonghyeon-paper <lieniece@gmail.com>
2025-03-05 14:51:57 +0900

    Create BusinessException.java

---
+++ src/main/java/zz/yy/xx/exception/BusinessException.java
@@ -0,0 +1,44 @@
+package zz.yy.xx.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.EqualsAndHashCode;
+import zz.yy.xx.core.attribute.SimpleResponse;
+
+/**
+ * BusinessException:
+ * 
+ * <br>
+ * created by good on 2025.03.
+ */
+@EqualsAndHashCode(callSuper = false)
+public class BusinessException extends RuntimeException implements SimpleResponse {
+
+    private static final long serialVersionUID = 9182664201848370802L;
+
+    private SimpleResponse simpleResponse;
+
+    public BusinessException(SimpleResponse simpleResponse) {
+        this.simpleResponse = simpleResponse;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return simpleResponse.getHttpStatus();
+    }
+
+    @Override
+    public String getBody() {
+        return simpleResponse.getBody();
+    }
+
+    @Override
+    public String getExtraCode() {
+        return simpleResponse.getExtraCode();
+    }
+
+    @Override
+    public String getExtraMessage() {
+        return simpleResponse.getExtraMessage();
+    }
+}
